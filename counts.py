2a3e6bc72bff10ad1eace574adc7f520f7553db8
Chris Zielinski <ckz8780@gmail.com>
2018-05-21 23:52:33 +0000

    updated assertions

--- counts.py
+++ counts.py
@@ -5,9 +5,11 @@ def count_upper_case(message):
             count += 1
     return count
 
-assert count_upper_case("") == 0, "Empty string"
-assert count_upper_case("A") == 1, "One upper case"
-assert count_upper_case("a") == 0, "One lower case"
-assert count_upper_case("£$%%^") == 0, "Special characters"
+assert count_upper_case("") == 0, "count_upper_case(\"\") should have returned 0, but didn't"
+assert count_upper_case("A") == 1, "count_upper_case(\"A\") should have returned 1, but didn't"
+assert count_upper_case("a") == 0, "count_upper_case(\"a\") should have returned 0, but didn't"
+assert count_upper_case("£$%%^") == 0, "count_upper_case(\"£$%%^\") should have returned 0, but didn't"
+assert count_upper_case("AbC") == 2, "count_upper_case(\"AbC\") should have returned 2, but didn't"
+
 
 print("All the tests passed")
